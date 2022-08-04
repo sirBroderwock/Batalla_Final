8a620daf545e9a87e74a2e57ce1ab20c5ae62b5d
Sergio Gonzalo Rodriguez Martinez <sergiorodriguez001@gmail.com>
2022-08-04 17:24:56 -0500

    Se creo nuevo branch DOM

--- javascript/java.js
+++ javascript/java.js
@@ -243,48 +243,48 @@
 
 /*Tienda Mágica*/
 
-class pociones {
-  constructor(numeroId, nombre, precio, cantidad, efecto) {
-    this.numeroId = numeroId;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.cantidad = cantidad;
-    this.efecto = efecto;
-  }
-}
+// class pociones {
+//   constructor(numeroId, nombre, precio, cantidad, efecto) {
+//     this.numeroId = numeroId;
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.cantidad = cantidad;
+//     this.efecto = efecto;
+//   }
+// }
 
-const pocion__Vida = new pociones(1, "Pocion de Vida", 20, 5, +50);
-const pocion__Defensa = new pociones(2, "Pocion de Defensa", 40, 5, +5);
-const pocion__Fuerza = new pociones(3, "Pocion de Fuerza", 40, 5, +5);
+// const pocion__Vida = new pociones(1, "Pocion de Vida", 20, 5, +50);
+// const pocion__Defensa = new pociones(2, "Pocion de Defensa", 40, 5, +5);
+// const pocion__Fuerza = new pociones(3, "Pocion de Fuerza", 40, 5, +5);
 
-const tienda = [pocion__Vida, pocion__Defensa, pocion__Fuerza];
+// const tienda = [pocion__Vida, pocion__Defensa, pocion__Fuerza];
 
-console.log(tienda);
+// console.log(tienda);
 
-const tiendaNombres = tienda.map((el) => {
-  return {
-    nombre: el.nombre,
-    precio: el.precio,
-  };
-});
+// const tiendaNombres = tienda.map((el) => {
+//   return {
+//     nombre: el.nombre,
+//     precio: el.precio,
+//   };
+// });
 
-console.log(tiendaNombres);
+// console.log(tiendaNombres);
 
-let alertaNombres;
+// let alertaNombres;
 
-for (tiendaNombresLista of tiendaNombres) {
-  alertaNombres +=
-    "\n" +
-    tiendaNombresLista.nombre +
-    " - " +
-    tiendaNombresLista.precio +
-    " oro" +
-    "\n";
-}
+// for (tiendaNombresLista of tiendaNombres) {
+//   alertaNombres +=
+//     "\n" +
+//     tiendaNombresLista.nombre +
+//     " - " +
+//     tiendaNombresLista.precio +
+//     " oro" +
+//     "\n";
+// }
 
-alert(
-  "De pronto te encuentras con una tienda que vende:" + "\n" + alertaNombres
-);
+// alert(
+//   "De pronto te encuentras con una tienda que vende:" + "\n" + alertaNombres
+// );
 
 // for (const listaTienda of tienda) {
 //   const listaTiendaNombres = listaTienda.nombre;
