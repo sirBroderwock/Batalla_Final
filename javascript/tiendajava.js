766326a6b765bb6137d7238daeb3022361e0aed3
Crono <crono@Sergios-MacBook-Air.local>
2022-08-21 22:35:07 -0500

    se edito tiendita

--- javascript/tiendajava.js
+++ javascript/tiendajava.js
@@ -1,60 +1,60 @@
 /*Tienda Mágica*/
 
-// class pociones {
-//   constructor(numeroId, nombre, precio, cantidad, efecto) {
-//     this.numeroId = numeroId;
-//     this.nombre = nombre;
-//     this.precio = precio;
-//     this.cantidad = cantidad;
-//     this.efecto = efecto;
-//   }
-// }
+class pociones {
+  constructor(numeroId, nombre, precio, cantidad, efecto) {
+    this.numeroId = numeroId;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+    this.efecto = efecto;
+  }
+}
 
-// const pocion__Vida = new pociones(1, "Pocion de Vida", 20, 5, +50);
-// const pocion__Defensa = new pociones(2, "Pocion de Defensa", 40, 5, +5);
-// const pocion__Fuerza = new pociones(3, "Pocion de Fuerza", 40, 5, +5);
+const pocion__Vida = new pociones(1, "Pocion de Vida", 20, 5, +50);
+const pocion__Defensa = new pociones(2, "Pocion de Defensa", 40, 5, +5);
+const pocion__Fuerza = new pociones(3, "Pocion de Fuerza", 40, 5, +5);
 
-// const tienda = [pocion__Vida, pocion__Defensa, pocion__Fuerza];
+const tienda = [pocion__Vida, pocion__Defensa, pocion__Fuerza];
 
-// console.log(tienda);
+console.log(tienda);
 
-// const tiendaNombres = tienda.map((el) => {
-//   return {
-//     nombre: el.nombre,
-//     precio: el.precio,
-//   };
-// });
+const tiendaNombres = tienda.map((el) => {
+  return {
+    nombre: el.nombre,
+    precio: el.precio,
+  };
+});
 
-// console.log(tiendaNombres);
+console.log(tiendaNombres);
 
-// let alertaNombres;
+let alertaNombres;
 
-// for (tiendaNombresLista of tiendaNombres) {
-//   alertaNombres +=
-//     "\n" +
-//     tiendaNombresLista.nombre +
-//     " - " +
-//     tiendaNombresLista.precio +
-//     " oro" +
-//     "\n";
-// }
+for (tiendaNombresLista of tiendaNombres) {
+  alertaNombres +=
+    "\n" +
+    tiendaNombresLista.nombre +
+    " - " +
+    tiendaNombresLista.precio +
+    " oro" +
+    "\n";
+}
 
-// alert(
-//   "De pronto te encuentras con una tienda que vende:" + "\n" + alertaNombres
-// );
+alert(
+  "De pronto te encuentras con una tienda que vende:" + "\n" + alertaNombres
+);
 
-// for (const listaTienda of tienda) {
-//   const listaTiendaNombres = listaTienda.nombre;
-//   listaTiendaNombres1 = listaTiendaNombres.concat("\n");
-// }
-// alert(listaTiendaNombres1);
+for (const listaTienda of tienda) {
+  const listaTiendaNombres = listaTienda.nombre;
+  listaTiendaNombres1 = listaTiendaNombres.concat("\n");
+}
+alert(listaTiendaNombres1);
 
-// alert(
-//   "De pronto te encuentras con un " +
-//     dragon.nombre +
-//     " con " +
-//     dragon.hitPoints +
-//     " HP de vida.  Además de eso, ves que su poder de batalla es " +
-//     dragon.poderMagico +
-//     " y eso, te espanta mucho."
-// );
+alert(
+  "De pronto te encuentras con un " +
+    dragon.nombre +
+    " con " +
+    dragon.hitPoints +
+    " HP de vida.  Además de eso, ves que su poder de batalla es " +
+    dragon.poderMagico +
+    " y eso, te espanta mucho."
+);
