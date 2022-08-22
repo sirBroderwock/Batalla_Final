4b7e277c1756a7b42aa19b9297efb88787d87971
Sergio Gonzalo Rodriguez Martinez <sergiorodriguez001@gmail.com>
2022-08-21 19:49:29 -0500

    starting sweet alert things

--- javascript/java.js
+++ javascript/java.js
@@ -71,6 +71,20 @@ botonEmpezar.onclick = () => {
 
 //Botones Reset
 
+function alert__Reload__Sweet() {
+  Swal.fire({
+    title: "¿Estas seguro de que quieres reiniciar?",
+    showCancelButton: true,
+    confirmButtonText: "Si",
+    cancelButtonText: "No",
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      reload();
+    }
+  });
+}
+
 function reload() {
   reload = location.reload();
 }
@@ -101,7 +115,7 @@ let botonReiniciar__P = document.createElement("p");
 botonReiniciar__P.innerHTML = "¿Estas seguro de que quieres empezar de nuevo?";
 botonReiniciar__P.id = "botonReiniciar__P";
 
-botonReiniciar.addEventListener("click", botonReiniciar__Empezar, false);
+botonReiniciar.addEventListener("click", alert__Reload__Sweet(), false);
 
 botonReiniciar__Si.addEventListener("click", reload, false);
 
@@ -715,64 +729,3 @@ function enemigosJuego() {
   console.log(dragon);
   heroeStorageMain.push(dragon);
 }
-
-/*Tienda Mágica*/
-
-// class pociones {
-//   constructor(numeroId, nombre, precio, cantidad, efecto) {
-//     this.numeroId = numeroId;
-//     this.nombre = nombre;
-//     this.precio = precio;
-//     this.cantidad = cantidad;
-//     this.efecto = efecto;
-//   }
-// }
-
-// const pocion__Vida = new pociones(1, "Pocion de Vida", 20, 5, +50);
-// const pocion__Defensa = new pociones(2, "Pocion de Defensa", 40, 5, +5);
-// const pocion__Fuerza = new pociones(3, "Pocion de Fuerza", 40, 5, +5);
-
-// const tienda = [pocion__Vida, pocion__Defensa, pocion__Fuerza];
-
-// console.log(tienda);
-
-// const tiendaNombres = tienda.map((el) => {
-//   return {
-//     nombre: el.nombre,
-//     precio: el.precio,
-//   };
-// });
-
-// console.log(tiendaNombres);
-
-// let alertaNombres;
-
-// for (tiendaNombresLista of tiendaNombres) {
-//   alertaNombres +=
-//     "\n" +
-//     tiendaNombresLista.nombre +
-//     " - " +
-//     tiendaNombresLista.precio +
-//     " oro" +
-//     "\n";
-// }
-
-// alert(
-//   "De pronto te encuentras con una tienda que vende:" + "\n" + alertaNombres
-// );
-
-// for (const listaTienda of tienda) {
-//   const listaTiendaNombres = listaTienda.nombre;
-//   listaTiendaNombres1 = listaTiendaNombres.concat("\n");
-// }
-// alert(listaTiendaNombres1);
-
-// alert(
-//   "De pronto te encuentras con un " +
-//     dragon.nombre +
-//     " con " +
-//     dragon.hitPoints +
-//     " HP de vida.  Además de eso, ves que su poder de batalla es " +
-//     dragon.poderMagico +
-//     " y eso, te espanta mucho."
-// );
